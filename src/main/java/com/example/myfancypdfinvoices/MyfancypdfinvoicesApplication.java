b177a4450eb8a7e81de61b40ef5632d0ca15d241
Simon Vargas <sjvargas01@gmail.com>
2022-08-21 01:21:44 -0400

    first commit

---
+++ src/main/java/com/example/myfancypdfinvoices/MyfancypdfinvoicesApplication.java
@@ -0,0 +1,13 @@
+package com.example.myfancypdfinvoices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfancypdfinvoicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfancypdfinvoicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myfancypdfinvoices/MyfancypdfinvoicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myfancypdfinvoices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyfancypdfinvoicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
