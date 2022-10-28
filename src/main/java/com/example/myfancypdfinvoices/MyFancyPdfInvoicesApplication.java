d31f1234133c5d57ae7f8f5d42e30ed9715e9aed
Simon Vargas <sjvargas01@gmail.com>
2022-10-27 22:46:55 -0400

    renamed files

--- src/main/java/com/example/myfancypdfinvoices/MyFancyPdfInvoicesApplication.java
+++ src/main/java/com/example/myfancypdfinvoices/MyFancyPdfInvoicesApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyfancypdfinvoicesApplication {
+public class MyFancyPdfInvoicesApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyfancypdfinvoicesApplication.class, args);
+
+        SpringApplication.run(MyFancyPdfInvoicesApplication.class, args);
     }
 
 }
